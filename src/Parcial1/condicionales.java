b8847d44be26fe7b2c53ec9dd2c462c0aa75189f
Javier Alejandro Ramirez Cruz <javier.alexrc@gmail.com>
2026-09-02 17:29:15 -0600

    Proyecto

--- src/Parcial1/condicionales.java
+++ src/Parcial1/condicionales.java
@@ -1,8 +1,12 @@
 package Parcial1;
+import java.util.Scanner;
 
 public class condicionales {
     static void main() {
 
+
+
+        System.exit(0);
         int edad = 23;
         if (edad >= 18){
             System.out.println("Puedes jugar: GTA 6, RE7, Silent Hill 2");
---
+++ src/Parcial1/estructurasControl.java
@@ -0,0 +1,26 @@
+package Parcial1;
+import java.util.Scanner;
+
+public class estructurasControl {
+    static void main() {
+        Scanner leer = new Scanner(System.in);
+
+        System.out.print("Cuanto aura tienes? ");
+        int aura = leer.nextInt();
+        System.out.print("Tienes memorias?");
+        boolean tienesMemorias = leer.nextBoolean();
+        if (aura >= 100 && tienesMemorias == false){
+            System.out.println("Bienvenido a tu primer startup");
+        } else {
+            System.out.println("No puedes pasar");
+        }
+
+        System.exit(0);
+
+        System.out.println(10 > 3);
+        System.out.println(10 < 3);
+        System.out.println(10 == 3);
+        System.out.println(3 == 3);
+
+    }
+}
