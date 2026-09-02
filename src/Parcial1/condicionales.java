b064393f3ffcc8f2689b84191603a6b2c51f0fe2
Javier Alejandro Ramirez Cruz <javier.alexrc@gmail.com>
2026-09-02 17:41:51 -0600

    Proyecto

--- src/Parcial1/condicionales.java
+++ src/Parcial1/condicionales.java
@@ -4,6 +4,18 @@ import java.util.Scanner;
 public class condicionales {
     static void main() {
 
+        // Retiro de saldo
+        Scanner leer = new Scanner(System.in);
+        double saldo = 4000.0, retiro;
+        System.out.print("No lo haga joven, cuanto va a retirar :(? ");
+        retiro = leer.nextDouble();
+        if (retiro <= saldo && retiro > 0){
+            saldo = saldo - retiro;
+            //saldo -= retiro;
+            System.out.println("Saldo nuevo pobre es: " + saldo);
+        } else {
+            System.out.println("Pobre saldo insuficiente, trabaja");
+        }
 
 
         System.exit(0);
