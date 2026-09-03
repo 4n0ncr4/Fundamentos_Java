f2091d98650f53514fb794cc98c3a2038b4117b3
Javier Alejandro Ramirez Cruz <javier.alexrc@gmail.com>
2026-09-02 18:26:13 -0600

    Proyecto

--- src/Parcial1/Elif.java
+++ src/Parcial1/Elif.java
@@ -4,6 +4,20 @@ import java.util.Scanner;
 public class Elif {
     static void main() {
         Scanner leer = new Scanner(System.in);
+        String tipoAmor;
+        System.out.println("Descubre la verdad amigo no cierres los ojos");
+        tipoAmor = leer.next();
+        if (tipoAmor.equals("Te ama")) {
+            System.out.println("Te ama");
+        } else if (tipoAmor.equals("Te quiere")) {
+            System.out.println("Te quiere");
+        } else if (tipoAmor.equals("Le gustas")) {
+            System.out.println("Le gustas");
+        } else {
+            System.out.println("ERES UN JUGUETE");
+        }
+
+        System.exit(0);
 
         System.out.println("1 Debito 2 Credito, 3 Efectivo");
         int opcion = leer.nextInt();
