68c064a0ae2c90052a33e4b8b06f740d13e2ab0f
Javier Alejandro Ramirez Cruz <javier.alexrc@gmail.com>
2026-09-04 18:24:22 -0600

    Proyecto

---
+++ src/Parcial1/IfTernario.java
@@ -0,0 +1,20 @@
+package Parcial1;
+
+import java.util.Scanner;
+
+public class IfTernario {
+    static void main() {
+        Scanner leer = new Scanner(System.in);
+        System.out.print("Dame tu password: ");
+        String password = leer.nextLine();
+        String buenaContraseña = password.length() >= 8 ? "Buena contraseña" : "Mala contraseña";
+        System.out.println(buenaContraseña);
+
+        System.exit(0);
+
+        String tipo = leer.nextLine();
+        String serie = (tipo.equals("anime")) ? "Los 7 pecados capitales" : "Supernatural";
+        System.out.println(serie);
+
+    }
+}
