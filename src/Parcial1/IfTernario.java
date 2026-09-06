466fd7fd7c84dd7279a1e7b11c12249611de0ad4
Javier Alejandro Ramirez Cruz <javier.alexrc@gmail.com>
2026-09-06 13:00:29 -0600

    Updated

--- src/Parcial1/IfTernario.java
+++ src/Parcial1/IfTernario.java
@@ -4,7 +4,35 @@ import java.util.Scanner;
 public class IfTernario {
     static void main() {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Cuál es el nombre del producto? ");
+        String nombreProducto = scanner.nextLine();
 
+        System.out.print("Qué cantidad de " +  nombreProducto + " hay disponible? ");
+        int cantidadDisponible = scanner.nextInt();
+
+        String disponibilidad = cantidadDisponible > 0 ? "Disponible" : "Agotado";
+        System.out.println(disponibilidad);
+
+        /* (PROGRAMA EXTRA)
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Cuál fue tu calificación este parcial? ");
+        int calificacion = sc.nextInt();
+
+        if (calificacion < 0 || calificacion > 100){
+            System.out.println("Favor de ingresar un número válido");
+        } else {
+            String rangoCalificaciones =
+                    calificacion <= 100 && calificacion >= 90
+                    ? "Excelente, sigue así" :
+                    calificacion <= 89 && calificacion >= 80
+                    ? "Muy bien" :
+                    calificacion <= 79 && calificacion >= 70
+                    ? "Se puede mejorar":
+                    "Reprobado, practica más";
+            System.out.println(rangoCalificaciones);
+        }
+        */
 
         /* EJERCICIOS DE CLASE
         Scanner leer = new Scanner(System.in);
